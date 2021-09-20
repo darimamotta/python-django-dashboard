897a058d7c537def877e63b0eebc9dbf8ce43b40
Darima Motta <darima_mtp@mail.ru>
2021-09-21 01:35:27 +0300

    Dashboard Pie Charts

---
+++ myproject/myproject/views.py
@@ -0,0 +1,7 @@
+from django.contrib import auth
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
